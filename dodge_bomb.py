a2bb1670f1f3c64aa2135ea1dbdd0d640a2fb407
c0a2423875 <c0a2423875@edu.teu.ac.jp>
2025-04-22 14:48:47 +0900

    三回目のコミット

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,6 +13,21 @@ DELTA = {
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRectまたは爆弾Rect
+    戻り値：判定結果（横、縦）
+    画面外ならTrue、画面外ならFalse
+    """
+    yoko, tate = True, True  #  横, 縦判定用変数
+    #左右判定
+    if rct.left < 0 or WIDTH < rct.right: # 横方向判定
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom: # 縦方向判定
+        tate = False
+    return yoko, tate
+
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -54,9 +69,16 @@ def main():
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):  #画面外だったら
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) #  画面内に戻す
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy)
-        screen.blit(bb_img, bb_rct)
+        bb_rct.move_ip(vx, vy)  #爆弾の速度
+        yoko, tate = check_bound(bb_rct)
+        if not yoko: #  左右どちらか
+            vx *= -1
+        if not tate: #  上下どちらか
+            vy *= -1
+        screen.blit(bb_img, bb_rct)  #爆弾の描画
         pg.display.update()
         tmr += 1
         clock.tick(50)
